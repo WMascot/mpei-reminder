2c10fde5208d4bbc28e6b7c01e0863f1a8f01bcd
WMascot <danila33366@gmail.com>
2023-08-21 12:55:11 +0300

    created BLL-service register

---
+++ MPEI_Reminder.BLL/ServicesRegister.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MPEI_Reminder.BLL.Services;
+
+namespace MPEI_Reminder.BLL
+{
+    public static class ServicesRegister
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services) =>
+            services.AddScoped<CoursesService>()
+                    .AddScoped<SemestrDatesService>()
+                    .AddScoped<StudentsService>()
+                    .AddScoped<ProfessorsService>()
+            ;
+    }
+}
