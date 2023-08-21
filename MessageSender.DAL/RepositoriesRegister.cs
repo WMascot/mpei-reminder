04bf58a3bbed0ecf13de9a1e1e269fa35994ce5e
WMascot <danila33366@gmail.com>
2023-08-21 09:36:26 +0300

    created service register

---
+++ MessageSender.DAL/RepositoriesRegister.cs
@@ -0,0 +1,17 @@
+﻿using MessageSender.DAL.Entities;
+using MessageSender.DAL.Interfaces;
+using MessageSender.DAL.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MessageSender.DAL
+{
+    public static class RepositoriesRegister
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services) =>
+            services.AddScoped<IRepository<Course>, CoursesRepository>()
+                    .AddScoped<IRepository<SemestrDate>, SemestrDatesRepository>()
+                    .AddScoped<IRepository<Student>, StudentsRepository>()
+                    .AddScoped<IRepository<Professor>, ProfessorsRepository>()
+            ;
+    }
+}
